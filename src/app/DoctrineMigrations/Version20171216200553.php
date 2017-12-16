fd46de29e3b10921bdc2fffc2cce6b04f7785410
Viktoria Simsikova <viktoria.simsikova@outlook.com>
2017-12-16 21:26:21 +0100

    Issue #45 - fixed migrations

--- src/app/DoctrineMigrations/Version20171216200553.php
+++ src/app/DoctrineMigrations/Version20171216200553.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20171211231630 extends AbstractMigration
+class Version20171216200553 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
