0a1cf3f650c2efca65d75323e5ab7b3c72e99c3b
Zoltan Onody <zoltan.onody@gmail.com>
2017-12-28 21:32:04 +0100

    Issue #45 - Fix wrong migration

--- src/app/DoctrineMigrations/Version20171216200553.php
+++ src/app/DoctrineMigrations/Version20171216200553.php
@@ -16,8 +16,6 @@ class Version20171216200553 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, language VARCHAR(50) NOT NULL, code VARCHAR(6) NOT NULL, created DATETIME NOT NULL, updated DATETIME DEFAULT NULL, deleted DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_D4DB71B5D4DB71B5 (language), UNIQUE INDEX UNIQ_D4DB71B577153098 (code), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE email_log_content ADD created DATETIME NOT NULL, ADD updated DATETIME DEFAULT NULL, ADD deleted DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE email_log_status ADD created DATETIME NOT NULL, ADD updated DATETIME DEFAULT NULL, ADD deleted DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema)
@@ -26,7 +24,5 @@ class Version20171216200553 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE language');
-        $this->addSql('ALTER TABLE email_log_content DROP created, DROP updated, DROP deleted');
-        $this->addSql('ALTER TABLE email_log_status DROP created, DROP updated, DROP deleted');
     }
 }
