933b863d4252c7514edfe5d9d9215a27a899de8e
Zoltan Onody <zoltan.onody@gmail.com>
2018-02-12 16:55:15 +0100

    Issue #106 - Better random data

--- src/src/AdminBundle/Command/AddMockedDataCommand.php
+++ src/src/AdminBundle/Command/AddMockedDataCommand.php
@@ -18,6 +18,24 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class AddMockedDataCommand extends ContainerAwareCommand
 {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $this->createAdmins($em);
+        $this->createLanguages($em);
+        $this->createOrganizers($em);
+        $this->createAttendees($em);
+        $this->createEventTypes($em);
+        $this->createEvents($em);
+
+        $this->assignOrganizersToEvents($em);
+        $this->assignLanguagesToEvents($em);
+        $this->assignLanguagesToEventTypes($em);
+
+        $this->createRegistrations($em);
+    }
+
     private function randomFirstName()
     {
         $names = array(
@@ -47,45 +65,38 @@ class AddMockedDataCommand extends ContainerAwareCommand
         return $surnames[mt_rand(0, sizeof($surnames) - 1)];
     }
 
-    private function randomDate()
+    /*
+     * $after = DateTime
+     * $offset_from =  min offset + $after
+     * $offset_to = max offset + $after
+     */
+    private function randomDate($after = null, $offset_from = 0, $offset_to = 126144000)
     {
+        if (null == $after) {
+            $timestamp_from = 1451606400;
+        } else {
+            $timestamp_from = $after->getTimestamp();
+        }
+
         $random = new \DateTime();
-        $random->setTimestamp(mt_rand(1202055681, 1282055681));
+        $random->setTimestamp(mt_rand($timestamp_from + $offset_from, $timestamp_from + $offset_to));
 
         return $random;
     }
 
     private function getRandomObject($em, $objectType)
     {
+        return $this->getRandomObjects($em, $objectType, 1)[0];
+    }
+
+    private function getRandomObjects($em, $objectType, $count)
+    {
         $data = $em->getRepository($objectType)->findAll();
+
+        $count = min($count, count($data));
         shuffle($data);
 
-        return $data[0];
-    }
-
-    private function getRandomLanguage($em)
-    {
-        return $this->getRandomObject($em, Language::class);
-    }
-
-    private function getRandomEvent($em)
-    {
-        return $this->getRandomObject($em, Event::class);
-    }
-
-    private function getRandomAttendee($em)
-    {
-        return $this->getRandomObject($em, Attendee::class);
-    }
-
-    private function getRandomOrganizer($em)
-    {
-        return $this->getRandomObject($em, Organizer::class);
-    }
-
-    private function getRandomEventType($em)
-    {
-        return $this->getRandomObject($em, EventType::class);
+        return array_slice($data, 0, $count);
     }
 
     private function makeEmail($first, $last)
@@ -104,25 +115,6 @@ class AddMockedDataCommand extends ContainerAwareCommand
             ->setDescription('Adds mocked data for development purposes');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $em = $this->getContainer()->get('doctrine')->getManager();
-
-        $this->createAdmins($em);
-        $this->createLanguages($em);
-        $this->createOrganizers($em);
-        $this->createAttendees($em);
-        $this->createEventTypes($em);
-        $this->createEvents($em);
-        $this->createRegistrations($em);
-
-        $this->assignOrganizersToEvents($em);
-        $this->assignLanguagesToEvents($em);
-        $this->assignLanguagesToEventTypes($em);
-
-        $em->flush();
-    }
-
     private function createAdmins($em)
     {
         for ($i = 0; $i < 10; ++$i) {
@@ -131,20 +123,26 @@ class AddMockedDataCommand extends ContainerAwareCommand
             $admin->setPassword('');
             $em->persist($admin);
         }
+        $em->flush();
     }
 
     private function createLanguages($em)
     {
-        $lang1 = new Language();
-        $lang1->setCode('en_US');
-        $lang1->setLanguage('English');
+        $data = [
+            array('code' => 'en_US', 'lang' => 'English'),
+            array('code' => 'sk_SK', 'lang' => 'Slovak'),
+            array('code' => 'es_ES', 'lang' => 'Spanish'),
+            array('code' => 'hu_HU', 'lang' => 'Hungarian'),
+            array('code' => 'cz_CZ', 'lang' => 'Czech'),
+        ];
 
-        $lang2 = new Language();
-        $lang2->setCode('sk_SK');
-        $lang2->setLanguage('Slovak');
-
-        $em->persist($lang1);
-        $em->persist($lang2);
+        foreach ($data as $lang) {
+            $language = new Language();
+            $language->setCode($lang['code']);
+            $language->setLanguage($lang['lang']);
+            $em->persist($language);
+        }
+        $em->flush();
     }
 
     private function createOrganizers($em)
@@ -154,6 +152,7 @@ class AddMockedDataCommand extends ContainerAwareCommand
             $organizer->setEmail($this->randomEmail());
             $em->persist($organizer);
         }
+        $em->flush();
     }
 
     private function createAttendees($em)
@@ -162,11 +161,12 @@ class AddMockedDataCommand extends ContainerAwareCommand
             $attendee = new Attendee();
             $attendee->setFirstName($this->randomFirstName());
             $attendee->setLastName($this->randomLastName());
-            $attendee->setLanguages($this->getRandomLanguage($em));
+            $attendee->setLanguages($this->getRandomObject($em, Language::class));
             $attendee->setUnsubscribed((bool) random_int(0, 1));
             $attendee->setEmail($this->makeEmail($attendee->getFirstName(), $attendee->getLastName()));
             $em->persist($attendee);
         }
+        $em->flush();
     }
 
     private function createEventTypes($em)
@@ -174,83 +174,115 @@ class AddMockedDataCommand extends ContainerAwareCommand
         for ($i = 0; $i < 5; ++$i) {
             $type = new EventType();
             $type->setTemplate('default');
-            $type->setSlug(strtolower($this->randomFirstName()));
+            $type->setSlug(strtolower($this->randomFirstName().random_int(0, 100)));
             $em->persist($type);
         }
+        $em->flush();
     }
 
     private function createEvents($em)
     {
-        for ($i = 0; $i < 10; ++$i) {
-            $event = new Event();
-            $event->setAddress('Kosice'.$i);
-            $event->setCapacity(random_int(10, 200));
-            $event->setDateTime($this->randomDate());
-            $event->setEventType($this->getRandomEventType($em));
-            $event->setRegistrationStart($this->randomDate());
-            $event->setRegistrationEnd($this->randomDate());
-            $event->setTemplateOverride('default');
-            $event->setNotificationThreshold(random_int(1, 7));
+        $cities = ['Kosice', 'Bratislava', 'Nitra', 'Banska Bistrica'];
+        $capacity = [100, 200, 20, 50];
 
-            $em->persist($event);
+        $eventTypes = $em->getRepository(EventType::class)->findAll();
+        foreach ($eventTypes as $eventType) {
+            for ($i = 0; $i < random_int(0, 4); ++$i) {
+                $event = new Event();
+                $event->setAddress($cities[$i]);
+                $event->setCapacity($capacity[$i]);
+                $event->setEventType($eventType);
+                $event->setTemplateOverride('default');
+                $event->setNotificationThreshold(random_int(1, 7));
+
+                $registrationStart = $this->randomDate();
+                $registrationEnds = $this->randomDate($registrationStart, 60 * 60 * 24 * 7, 60 * 60 * 24 * 14);
+                $eventDate = $this->randomDate($registrationEnds, 60 * 60 * 24 * 2, 60 * 60 * 24 * 7);
+
+                $event->setDateTime($eventDate);
+                $event->setRegistrationStart($registrationStart);
+                $event->setRegistrationEnd($registrationEnds);
+
+                $em->persist($event);
+            }
         }
+        $em->flush();
     }
 
     private function createRegistrations($em)
     {
-        for ($i = 0; $i < 300; ++$i) {
-            $registration = new Registration();
-            $registration->setLanguages($this->getRandomLanguage($em));
-            $registration->setAttendee($this->getRandomAttendee($em));
-            $registration->setEvents($this->getRandomEvent($em));
+        $eventTypes = $em->getRepository(EventType::class)->findAll();
+        foreach ($eventTypes as $eventType) {
+            foreach ($eventType->getEvents() as $event) {
+                $attendees = $this->getRandomObjects($em, Attendee::class, random_int(0, $event->getCapacity()));
 
-            $registration->setCode(random_int(1000, 5000));
-            $registration->generateConfirmationToken();
+                foreach ($attendees as $attendee) {
+                    $registration = new Registration();
+                    $registration->setAttendee($attendee);
+                    $registration->setEvents($event);
+                    $registration->setCode(random_int(1000, 5000));
+                    $registration->generateConfirmationToken();
 
-            if (0 != random_int(0, 5)) {
-                $registration->setConfirmed(new \DateTime());
+                    if (0 != random_int(0, 5)) {
+                        $registration->setConfirmed(new \DateTime());
+                    }
+
+                    $eventLanguages = $em->getRepository(EventLanguages::class)->findBy(['event' => $event->getId()]);
+                    shuffle($eventLanguages);
+                    $registration->setLanguages($eventLanguages[0]->getLanguage());
+
+                    $em->persist($registration);
+                }
             }
-
-            $em->persist($registration);
         }
+        $em->flush();
     }
 
     private function assignOrganizersToEvents($em)
     {
         $events = $em->getRepository(Event::class)->findAll();
         foreach ($events as $event) {
-            for ($i = 0; $i < random_int(1, 5); ++$i) {
+            $organizers = $this->getRandomObjects($em, Organizer::class, random_int(1, 5));
+
+            foreach ($organizers as $organizer) {
                 $eo = new EventOrganizers();
                 $eo->setEvent($event);
-                $eo->setOrganizer($this->getRandomOrganizer($em));
+                $eo->setOrganizer($organizer);
                 $em->persist($eo);
             }
         }
+        $em->flush();
     }
 
     private function assignLanguagesToEvents($em)
     {
         $events = $em->getRepository(Event::class)->findAll();
         foreach ($events as $event) {
-            for ($i = 0; $i < random_int(1, 5); ++$i) {
+            $languages = $this->getRandomObjects($em, Language::class, random_int(1, 5));
+
+            foreach ($languages as $language) {
                 $el = new EventLanguages();
                 $el->setEvent($event);
-                $el->setLanguage($this->getRandomLanguage($em));
+                $el->setLanguage($language);
                 $em->persist($el);
             }
         }
+        $em->flush();
     }
 
     private function assignLanguagesToEventTypes($em)
     {
         $eventTypes = $em->getRepository(EventType::class)->findAll();
         foreach ($eventTypes as $eventType) {
-            for ($i = 0; $i < random_int(1, 5); ++$i) {
+            $languages = $this->getRandomObjects($em, Language::class, random_int(1, 5));
+
+            foreach ($languages as $language) {
                 $etl = new EventTypeLanguages();
                 $etl->setEventType($eventType);
-                $etl->setLanguage($this->getRandomLanguage($em));
+                $etl->setLanguage($language);
                 $em->persist($etl);
             }
         }
+        $em->flush();
     }
 }
