a4f290f0c40ac5f9832a30dc0113fa7f0dd647c6
Radovan Balog <radovan.balog@2ring.com>
2018-02-14 13:40:02 +0100

    Issue #128 - Validation for registration form

--- src/src/AppBundle/Form/RegistrationForm.php
+++ src/src/AppBundle/Form/RegistrationForm.php
@@ -9,15 +9,34 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegistrationForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('first_name', TextType::class)
-            ->add('last_name', TextType::class)
-            ->add('email', EmailType::class)
+            ->add('first_name', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 100)),
+                ),
+            ))
+            ->add('last_name', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 100)),
+                ),
+            ))
+            ->add('email', EmailType::class, array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 100)),
+                    new Email(),
+                ),
+            ))
             ->add('event_choice', ChoiceType::class, array(
                 'choices' => $options['data']['events'],
             ))
